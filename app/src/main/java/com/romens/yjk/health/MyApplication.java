c0f63f44b3394c2ec66ee311a4e481f088d7da7a
hzh-romens <huangzhihui@yaojk365.com>
2015-08-27 10:57:53 +0800

    HZH 更新GITHUB冲突

--- app/src/main/java/com/romens/yjk/health/MyApplication.java
+++ app/src/main/java/com/romens/yjk/health/MyApplication.java
@@ -7,6 +7,7 @@ import com.mobvoi.android.common.NoAvailableServiceException;
 import com.pgyersdk.crash.PgyCrashManager;
 import com.romens.android.AndroidUtilities;
 import com.romens.android.ApplicationLoader;
+import com.romens.android.log.FileLog;
 import com.romens.yjk.health.config.PgyConfig;
 
 /**
@@ -17,13 +18,11 @@ public class MyApplication extends ApplicationLoader {
     @Override
     public void onCreate() {
         super.onCreate();
-        //注册蒲公英 Crash接口
-        //蒲公英注册或上传应用获取的AppId
         PgyCrashManager.register(this, PgyConfig.APP_ID);
         try {
             MobvoiApiManager.getInstance().adaptService(applicationContext);
         } catch (NoAvailableServiceException e) {
-            e.printStackTrace();
+            FileLog.e("init tic api error", e);
         }
     }
 
