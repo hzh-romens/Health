499ea4ade94979546306d32ab97137bee58b0e79
hzh-romens <huangzhihui@yaojk365.com>
2015-11-26 14:47:10 +0800

    增加显示评论的部分

---
+++ app/src/main/java/com/romens/yjk/health/ui/cells/ADCommentCell.java
@@ -0,0 +1,30 @@
+package com.romens.yjk.health.ui.cells;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.romens.android.ui.Components.LayoutHelper;
+import com.romens.yjk.health.R;
+
+
+/**
+ * Created by HZH on 2015/11/26.
+ */
+public class ADCommentCell extends FrameLayout{
+    private TextView tv_comment,tv_name,tv_time;
+    private FlexibleRatingBar ratingBar;
+    public ADCommentCell(Context context) {
+        super(context);
+        View view=View.inflate(context,R.layout.list_item_comment,null);
+        tv_comment= (TextView) view.findViewById(R.id.tv_comment);
+        tv_name= (TextView) view.findViewById(R.id.tv_name);
+        tv_time= (TextView) view.findViewById(R.id.tv_time);
+        ratingBar= (FlexibleRatingBar) view.findViewById(R.id.ratingbar);
+        addView(view, LayoutHelper.createFrame(LayoutHelper.WRAP_CONTENT, LayoutHelper.MATCH_PARENT));
+    }
+    public void SetValue(String level,String commnet,String name,String time){
+        //设定值
+    }
+}
---
+++ app/src/main/java/com/romens/yjk/health/ui/controls/ADCommentControl.java
@@ -0,0 +1,35 @@
+package com.romens.yjk.health.ui.controls;
+
+import android.content.Context;
+
+import com.romens.yjk.health.ui.cells.ADCommentCell;
+import com.romens.yjk.health.ui.cells.ADHolder;
+
+/**
+ * Created by HZH on 2015/11/26.
+ */
+public class ADCommentControl extends ADBaseControl{
+    private String mLevel,mComment,mName,mTime;
+    public  ADCommentControl bindModle(String level,String commnet,String name,String time){
+        this.mComment=commnet;
+        this.mLevel=level;
+        this.mName=name;
+        this.mTime=time;
+        return this;
+    }
+    @Override
+    public void bindViewHolder(Context context, ADHolder holder) {
+        ADCommentCell adCommentCell= (ADCommentCell) holder.itemView;
+        //绑定值
+
+    }
+    public static ADHolder createViewHolder(Context context){
+        ADCommentCell adCommentCell=new ADCommentCell(context);
+        return new ADHolder(adCommentCell);
+    }
+
+    @Override
+    public int getType() {
+        return 14;
+    }
+}
