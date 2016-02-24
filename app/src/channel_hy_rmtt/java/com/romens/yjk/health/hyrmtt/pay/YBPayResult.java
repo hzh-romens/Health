51b6692f14b248128aebb08f1e62b90176ed832f
zls-romens <ls_zhou@romens.cn>
2016-02-24 18:05:23 +0800

    加入哈药医保支付返回页面

---
+++ app/src/channel_hy_rmtt/java/com/romens/yjk/health/hyrmtt/pay/YBPayResult.java
@@ -0,0 +1,18 @@
+package com.romens.yjk.health.hyrmtt.pay;
+
+import android.os.Bundle;
+
+import com.romens.yjk.health.ui.activity.BaseActionBarActivityWithAnalytics;
+
+/**
+ * @author Zhou Lisi
+ * @create 16/2/24
+ * @description
+ */
+public class YBPayResult extends BaseActionBarActivityWithAnalytics {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
