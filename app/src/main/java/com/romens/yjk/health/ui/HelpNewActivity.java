c45d2b41bb1a64eb6abc0da4ad1ad2da6ec2076b
zls-romens <ls_zhou@romens.cn>
2016-03-11 09:38:47 +0800

    修改帮助页

--- app/src/main/java/com/romens/yjk/health/ui/HelpNewActivity.java
+++ app/src/main/java/com/romens/yjk/health/ui/HelpNewActivity.java
@@ -1,32 +1,27 @@
 package com.romens.yjk.health.ui;
 
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 import com.romens.android.ui.ActionBar.ActionBar;
-import com.romens.android.ui.ActionBar.ActionBarLayout;
-import com.romens.android.ui.Components.LayoutHelper;
+import com.romens.yjk.health.R;
+import com.romens.yjk.health.config.ResourcesConfig;
+import com.romens.yjk.health.config.UserConfig;
+import com.romens.yjk.health.ui.activity.WebActivity;
 
 /**
  * Created by anlc on 2016/2/25.
  */
-public class HelpNewActivity extends BaseActivity {
-
-    private WebView webView;
-
+public class HelpNewActivity extends WebActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActionBarLayout.LinearLayoutContainer content = new ActionBarLayout.LinearLayoutContainer(this);
-        ActionBar actionBar = new ActionBar(this);
-        content.addView(actionBar, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
-        setContentView(content, actionBar);
-        actionBar.setTitle("帮助");
+
+        ActionBar actionBar = getMyActionBar();
+        setActionBarColor(actionBar, ResourcesConfig.primaryColor);
+        actionBar.setTitle("帮助", 0xffffffff);
+        actionBar.setBackButtonImage(R.drawable.ic_arrow_back_white_24dp);
         actionBar.setActionBarMenuOnItemClick(new ActionBar.ActionBarMenuOnItemClick() {
             public void onItemClick(int i) {
                 if (i == -1) {
@@ -34,55 +29,29 @@ public class HelpNewActivity extends BaseActivity {
                 }
             }
         });
-
-        needShowProgress("正在加载...");
-
-        if (!isConnected()) {
-            Toast.makeText(this, "请连接网络!", Toast.LENGTH_SHORT).show();
-        }
-
-        webView = new WebView(this);
-        webView.setVerticalScrollBarEnabled(false);
-        webView.setHorizontalScrollBarEnabled(false);
-
-        webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setLoadWithOverviewMode(true);
-
-        content.addView(webView, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
-        webView.loadUrl("http://115.28.244.190/help/help.html");
-
+        WebView webView = getWebView();
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                webView.loadUrl(url);
+                view.loadUrl(url);
                 return true;
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                needHideProgress();
             }
         });
-    }
 
-    private boolean isConnected() {
-        ConnectivityManager manager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-        NetworkInfo[] infos = manager.getAllNetworkInfo();
-        for (int i = 0; i < infos.length; i++) {
-            if (infos[i].getState() == NetworkInfo.State.CONNECTED) {
-                return true;
-            }
-        }
-        return false;
+        webView.loadUrl(String.format("http://115.28.244.190/help/help.html?org=%s", UserConfig.getOrgCode()));
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
-            webView.goBack();
-            return true;
+    protected void onWebPageCompleted() {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getWebView().canGoBack()) {
+            getWebView().goBack();
+        } else {
+            super.onBackPressed();
         }
-        return super.onKeyDown(keyCode, event);
     }
 }
