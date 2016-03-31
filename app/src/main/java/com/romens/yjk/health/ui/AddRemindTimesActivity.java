7c05aca0912d6f578cf9cb45606ade009b1a39ba
cxcxckx <15764231308@163.com>
2016-03-31 16:36:52 +0800

    用药提醒 服药时间修改删除方式

--- app/src/main/java/com/romens/yjk/health/ui/AddRemindTimesActivity.java
+++ app/src/main/java/com/romens/yjk/health/ui/AddRemindTimesActivity.java
@@ -10,9 +10,12 @@ import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.romens.android.AndroidUtilities;
 import com.romens.android.library.datetimepicker.time.RadialPickerLayout;
 import com.romens.android.library.datetimepicker.time.TimePickerDialog;
@@ -23,7 +26,10 @@ import com.romens.yjk.health.R;
 import com.romens.yjk.health.config.UserGuidConfig;
 import com.romens.yjk.health.model.TimesAdapterCallBack;
 import com.romens.yjk.health.ui.adapter.TimesAdapter;
+import com.romens.yjk.health.ui.components.SwipeDismissListView;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -31,7 +37,7 @@ import java.util.List;
  */
 public class AddRemindTimesActivity extends BaseActivity implements TimesAdapterCallBack {
 
-    private ListView listView;
+    private SwipeDismissListView listView;
     private TimesAdapter timesAdapter;
 
     private ArrayList<String> timesData;
@@ -48,14 +54,44 @@ public class AddRemindTimesActivity extends BaseActivity implements TimesAdapter
 
         initData();
 
-        listView = (ListView) findViewById(R.id.remind_add_time_list);
+        listView = (SwipeDismissListView) findViewById(R.id.remind_add_time_list);
         listView.setDivider(null);
         listView.setDividerHeight(0);
         listView.setVerticalScrollBarEnabled(false);
-        listView.addFooterView(new HintViewCell(this));
+//        listView.addFooterView(new HintViewCell(this));
         timesAdapter = new TimesAdapter(timesData, this, this);
         listView.setAdapter(timesAdapter);
         listView.setSelector(R.drawable.list_selector);
+        listView.setOnDismissCallback(new SwipeDismissListView.OnDismissCallback() {
+            @Override
+            public void onDismiss(int dismissPosition) {
+                //删除本条记录
+                if (timesData.size() > 1) {
+                    timesData.remove(dismissPosition);
+                    timesAdapter.notifyDataSetChanged();
+                } else {
+                   Toast.makeText(getApplicationContext(), "默认选项不能删除", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                Calendar calendar = Calendar.getInstance();
+                TimePickerDialog timePickerDialog = TimePickerDialog.newInstance(new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute) {
+                        String time = amendTime(hourOfDay) + ":" + amendTime(minute) ;
+                        timesData.remove(position) ;
+                        timesData.add(position,time);
+                        timesAdapter.notifyDataSetChanged();
+                    }
+                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false, false);
+                timePickerDialog.setVibrate(true);
+                timePickerDialog.setCloseOnSingleTapMinute(false);
+                timePickerDialog.show(AddRemindTimesActivity.this.getSupportFragmentManager(), "timepicker");
+            }
+        });
 
         FloatingActionButton actionButton = (FloatingActionButton) findViewById(R.id.remind_add_time_addbtn);
         actionButton.setImageResource(R.drawable.ic_add_white_24dp);
--- app/src/main/java/com/romens/yjk/health/ui/adapter/TimesAdapter.java
+++ app/src/main/java/com/romens/yjk/health/ui/adapter/TimesAdapter.java
@@ -1,31 +1,18 @@
 package com.romens.yjk.health.ui.adapter;
 
-import android.app.AlertDialog;
+
 import android.content.Context;
 import android.graphics.Color;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.romens.android.AndroidUtilities;
 import com.romens.android.library.datetimepicker.time.RadialPickerLayout;
 import com.romens.android.library.datetimepicker.time.TimePickerDialog;
-import com.romens.android.ui.Components.LayoutHelper;
 import com.romens.yjk.health.R;
 import com.romens.yjk.health.model.TimesAdapterCallBack;
-import com.romens.yjk.health.ui.BaseActivity;
-import com.romens.yjk.health.ui.components.SwLin;
-
-import java.util.Calendar;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by anlc on 2015/8/27.
@@ -38,13 +25,12 @@ public class TimesAdapter extends BaseAdapter implements TimePickerDialog.OnTime
     public static final String TIMEPICKER_TAG = "timepicker";
     private int index;
     private TimesAdapterCallBack dailog;
-    private Map<Integer, SwLin> mapView;
+
 
     public TimesAdapter(List<String> data, Context context, TimesAdapterCallBack callBack) {
         this.dailog = callBack;
         this.data = data;
         this.context = context;
-        mapView = new HashMap<Integer, SwLin>();
     }
 
     @Override
@@ -66,71 +52,16 @@ public class TimesAdapter extends BaseAdapter implements TimePickerDialog.OnTime
     public View getView(final int position, View convertView, ViewGroup parent) {
         TimesViewHolder holder = null;
         if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.list_item_swlin, null);
+            convertView = LayoutInflater.from(context).inflate(R.layout.list_item_add_time, null);
             holder = new TimesViewHolder();
-            holder.textView = (TextView) convertView.findViewById(R.id.swlin_time);
-            holder.textDelete = (TextView) convertView.findViewById(R.id.swlin_delete);
+            holder.textView = (TextView) convertView.findViewById(R.id.remind_add_time_text);
             convertView.setTag(holder);
         } else {
             holder = (TimesViewHolder) convertView.getTag();
         }
-        SwLin sw = (SwLin) convertView.findViewById(R.id.swlin_layout);
-        sw.setTag(position);
-        mapView.put(position, sw);
-        sw.setScreenListener(new SwLin.ScreenListener() {
 
-            @Override
-            public boolean startTouch(String tag) {
-                if (mTouch) {
-                    if (showMenuTag.equals(tag)) {
-                        mTouch = false;
-                    } else {
-                        int p = Integer.parseInt(showMenuTag);
-                        showMainLayout();
-                    }
-                }
-                return mTouch;
-            }
-
-            @Override
-            public void changeScreen(int screen, String tag) {
-                if (screen == 1) {
-                    mTouch = true;
-                    showMenuTag = tag;
-                }
-            }
-
-            @Override
-            public void canTouch(boolean flag) {
-                mTouch = false;
-            }
-
-        });
         holder.textView.setText(data.get(position));
-        holder.textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar calendar = Calendar.getInstance();
-                TimePickerDialog timePickerDialog = TimePickerDialog.newInstance(TimesAdapter.this, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false, false);
-                timePickerDialog.setVibrate(true);
-                timePickerDialog.setCloseOnSingleTapMinute(false);
-                timePickerDialog.show(((BaseActivity) context).getSupportFragmentManager(), TIMEPICKER_TAG);
-                index = position;
-            }
-        });
-        holder.textDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (data.size() > 1) {
-                    data.remove(position);
-                } else {
-                    Toast.makeText(context, "默认选项不能删除", Toast.LENGTH_SHORT).show();
-                }
-                showMainLayout();
-                notifyDataSetChanged();
 
-            }
-        });
         convertView.setBackgroundColor(Color.WHITE);
         return convertView;
     }
@@ -146,20 +77,8 @@ public class TimesAdapter extends BaseAdapter implements TimePickerDialog.OnTime
         notifyDataSetChanged();
     }
 
-    public void showMainLayout() {
-
-        for (int key : mapView.keySet()) {
-            mapView.get(key).showScreen(0);
-
-        }
-    }
-
-    private boolean mTouch = false;
-    private String showMenuTag;
-
     class TimesViewHolder {
         private TextView textView;
-        private TextView textDelete ;
     }
 }
 
