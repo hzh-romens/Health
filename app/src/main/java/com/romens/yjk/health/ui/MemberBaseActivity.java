48998ad03a5450c9fb09793692bfdbcbebd6fb3b
zls-romens <ls_zhou@romens.cn>
2016-04-13 19:10:11 +0800

    修改我的会员卡页面

--- app/src/main/java/com/romens/yjk/health/ui/MemberBaseActivity.java
+++ app/src/main/java/com/romens/yjk/health/ui/MemberBaseActivity.java
@@ -3,9 +3,12 @@ package com.romens.yjk.health.ui;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.romens.android.io.json.JacksonMapper;
@@ -24,6 +27,7 @@ import com.romens.yjk.health.R;
 import com.romens.yjk.health.config.FacadeConfig;
 import com.romens.yjk.health.config.FacadeToken;
 import com.romens.yjk.health.ui.base.DarkActionBarActivity;
+import com.romens.yjk.health.ui.components.ToastCell;
 import com.romens.yjk.health.ui.fragment.BindMemberFragment;
 import com.romens.yjk.health.ui.fragment.HomeHealthNewFragment;
 import com.romens.yjk.health.ui.fragment.MemberFragment;
@@ -34,18 +38,13 @@ import java.util.Map;
 /**
  * Created by HZH on 2016/3/24.
  */
-public abstract class MemberBaseActivity extends DarkActionBarActivity {
-    private FragmentTransaction fragmentTransaction;
-    private FragmentManager fragmentManager;
-    private ActionBar actionBar;
-
+public class MemberBaseActivity extends DarkActionBarActivity {
+    private ProgressBar progressBar;
     public Handler handler = new Handler() {
         @Override
         public void handleMessage(android.os.Message msg) {
             switch (msg.what) {
                 case 1:
-                    initFragmentManager();
-                    needShowProgress("正在加载");
                     JudgeMember();
                     break;
             }
@@ -55,11 +54,10 @@ public abstract class MemberBaseActivity extends DarkActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.memebr_base_layout);
-        actionBar = (ActionBar) findViewById(R.id.action_bar);
-        onSetupActionBar(actionBar);
-        needShowProgress("正在加载");
-        JudgeMember();
+        setContentView(R.layout.memebr_base_layout, R.id.action_bar);
+        progressBar = (ProgressBar) findViewById(R.id.progress);
+
+        ActionBar actionBar = getMyActionBar();
         ActionBarMenu menu = actionBar.createMenu();
         // menu.addItem(0, R.drawable.ic_add_white_24dp);
         actionBar.setActionBarMenuOnItemClick(new ActionBar.ActionBarMenuOnItemClick() {
@@ -79,10 +77,16 @@ public abstract class MemberBaseActivity extends DarkActionBarActivity {
 //                }
             }
         });
+        JudgeMember();
 
     }
 
+    private void showProgress(boolean progress) {
+        progressBar.setVisibility(progress ? View.VISIBLE : View.GONE);
+    }
+
     public void JudgeMember() {
+        showProgress(true);
         Map<String, String> args = new FacadeArgs.MapBuilder().
                 put("", "").build();
         FacadeProtocol protocol = new FacadeProtocol(FacadeConfig.getUrl(), "Handle", "GetUserMemberCard", args);
@@ -94,21 +98,18 @@ public abstract class MemberBaseActivity extends DarkActionBarActivity {
                         new Connect.AckDelegate() {
                             @Override
                             public void onResult(Message message, Message errorMessage) {
-                                needHideProgress();
+                                showProgress(false);
                                 if (errorMessage == null) {
                                     //2016-03-24 zhoulisi 使用JsonNode
                                     ResponseProtocol<JsonNode> responseProtocol = (ResponseProtocol) message.protocol;
                                     JsonNode jsonNode = responseProtocol.getResponse();
                                     try {
-
                                         String result = jsonNode.get("RESULT").asText();
                                         if (TextUtils.equals("0", result)) {
-                                            initFragmentManager();
                                             BindMemberFragment bindMemberFragment = new BindMemberFragment();
-                                            fragmentTransaction.add(R.id.layout_content, bindMemberFragment);
                                             setActionBarTitle("绑定会员卡");
+                                            setupFragment(bindMemberFragment);
                                         } else {
-                                            initFragmentManager();
                                             JsonNode dataObj = JacksonMapper.getInstance().readTree(jsonNode.get("DATA").textValue());
                                             JsonNode memberObj = dataObj.get("member").get(0);
                                             String lvLevel = memberObj.get("GROUPNAME").asText();//会员等级
@@ -127,15 +128,15 @@ public abstract class MemberBaseActivity extends DarkActionBarActivity {
                                             bundle.putString("diybgc", diybgc);
                                             bundle.putString("cardbackbg", cardbackbg);
                                             memberFragment.setArguments(bundle);
-                                            fragmentTransaction.add(R.id.layout_content, memberFragment);
                                             setActionBarTitle("我的会员卡");
+                                            setupFragment(memberFragment);
                                         }
-                                        fragmentTransaction.commit();
-                                        fragmentManager.executePendingTransactions();
-                                        needHideProgress();
                                     } catch (IOException e) {
                                         FileLog.e(e);
                                     }
+                                } else {
+                                    ToastCell.toast(MemberBaseActivity.this, "发生了一些错误,请稍后再试...");
+                                    finish();
                                 }
                             }
                         }
@@ -143,13 +144,16 @@ public abstract class MemberBaseActivity extends DarkActionBarActivity {
         ConnectManager.getInstance().request(this, connect);
     }
 
+    private void setupFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment, fragment).commit();
+    }
+
     public void setActionBarTitle(String title) {
-        actionBar.setTitle(title);
+        getMyActionBar().setTitle(title);
     }
 
-    public void initFragmentManager() {
-        fragmentManager = getSupportFragmentManager();
-        fragmentTransaction = fragmentManager.beginTransaction();
+    @Override
+    protected String getActivityName() {
+        return "我的会员卡";
     }
-
 }
